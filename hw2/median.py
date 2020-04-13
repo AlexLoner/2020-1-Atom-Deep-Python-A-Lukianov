a99a192e1ea9055e01929d3e7ca782a51c118eac
Aleksandr Lukianov <lukyanov9567@gmail.com>
2020-04-13 11:56:12 +0300

    ..

--- hw2/median.py
+++ hw2/median.py
@@ -2,17 +2,20 @@ import bisect
 
 class MedianFinder:
 
+    # ---------------------------------------------------------------------------------------------
     def __init__(self):
         """
         initialize your data structure here.
         """
         self.data = []
         self.pointer = -0.5
-
+    
+    # ---------------------------------------------------------------------------------------------
     def addNum(self, num: int) -> None:
         bisect.insort_left(self.data, num)
         self.pointer += 0.5
 
+    # ---------------------------------------------------------------------------------------------
     def findMedian(self) -> float:
         return self.data[int(self.pointer)] if self.pointer % 1 == 0.0 \
-            else 0.5 * (self.data[int(self.pointer - 0.5)] + self.data[int(self.pointer + 0.5)])
+            else 0.5 * (self.data[int(self.pointer - 0.5)] + self.data[int(self.pointer + 0.5)]
